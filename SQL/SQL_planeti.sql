17e7ae5d30b975eb4a32eb994f8d6cfaa554739b
LeonPuhanic <168485776+LeonPuhanic@users.noreply.github.com>
2024-05-14 18:49:06 +0200

    Popravak koda #2

--- SQL/SQL_planeti.sql
+++ SQL/SQL_planeti.sql
@@ -1,10 +1,13 @@
+use master;
+go
+drop database if exists SQLvjezba;
 create database SQLvjezba;
 go
 use SQLvjezba;
 
 create table planet(
-sifra int,
+sifra int primary key not null,
 naziv varchar(50),
 masa int,
 velicina int
-);
+)
