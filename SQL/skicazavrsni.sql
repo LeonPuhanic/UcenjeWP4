397275b525ad38b63731993b0d2c14bfd78ad8b8
Leon Puhanić <168485776+LeonPuhanic@users.noreply.github.com>
2024-05-16 16:20:17 +0200

    Dodavanje skice završnog rada na repozitorij

---
+++ SQL/skicazavrsni.sql
@@ -0,0 +1,54 @@
+use master;
+go
+drop database if exists specifikacijeNaoruzanja;
+go
+create database specifikacijeNaoruzanja;
+go
+
+use specifikacijeNaoruzanja
+
+create table Proizvodac(
+sifra int not null primary key,
+naziv varchar(100) not null,
+CEO varchar(100),
+datumosnovanja datetime not null,
+proizvodioruzje bit,
+proizvodimetke bit,
+proizvodioptike bit
+);
+
+create table Metci(
+sifra int not null primary key,
+naziv varchar(60) not null,
+cijena decimal(10,2),
+tezinazrna decimal(10,2),
+tezinametka decimal(10,2) not null,
+tip varchar(60),
+proizvodac int foreign key references proizvodac(sifra) not null
+);
+
+create table Oruzja(
+sifra int not null primary key,
+naziv varchar(60) not null,
+kalibar int not null foreign key references metci(sifra),
+cijena decimal(10,2),
+tezina int not null,
+proizvodac int foreign key references proizvodac(sifra) not null,
+duzina decimal(10,2),
+duzinacijevi decimal(10,2),
+brzinapaljbe int,
+pocetnabrzinametka int,
+kapacitetspremnika int,
+datumproizvodnje datetime not null,
+tippaljbe varchar(20) not null
+);
+
+create table Optike(
+sifra int not null primary key,
+naziv varchar(60) not null,
+cijena decimal(10,2),
+magnifikacija decimal(10,2),
+tezina decimal(10,2) not null,
+potrebnabaterija bit not null,
+proizvodac int foreign key references proizvodac(sifra) not null
+);
