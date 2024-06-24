3f14bf97921696b7e6a9a829cf2c8beff5aa7bd0
Leon Puhanić <168485776+LeonPuhanic@users.noreply.github.com>
2024-06-24 23:14:38 +0200

    Ciklična tablica nedovršeno

---
+++ CSHARP/Zadaca/Zadaca/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+Console.WriteLine("Unesite 2 broja za stvaranje tablice: ");
+
+
+int broj1 = int.Parse(Console.ReadLine());
+int broj2 = int.Parse(Console.ReadLine());
+
+
+
+for (int j = 1; j <= broj2; j++)
+{
+    for (int i = 1; i <= broj1; i++)
+    {
+        Console.Write("[" + i + "] ");
+    }
+    Console.WriteLine("");
+}
